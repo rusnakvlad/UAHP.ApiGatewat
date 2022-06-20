3f44c2e9cfb18a87a52337a315b44e43f456d730
Volodymyr Rusnak <vrusnak@oncologyanalytics.com>
2022-06-20 19:01:32 +0300

    added new gateways and cors

--- UAHause.APIGateway/Program.cs
+++ UAHause.APIGateway/Program.cs
@@ -5,9 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 builder.Configuration.AddJsonFile($"ocelot.{env}.json");
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
+app.UseCors("CorsPolicy");
+
 app.UseOcelot();
 
 app.Run();
